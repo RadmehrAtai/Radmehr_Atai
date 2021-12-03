8afcb6142804ddcc61b4b0a093dfb3bb5924cdc0
RadmehrAtai <92088576+RadmehrAtai@users.noreply.github.com>
2021-12-03 20:43:27 +0330

    Update files

--- scroll_to_top.js
+++ scroll_to_top.js
@@ -1,31 +1,4 @@
-import React, {useState} from "react";
 
-function ScrollToTop() {
-    const [isVisible, setIsVisible] = useState(false);
-
-    const toggleVisibility = () => {
-        const scrolled = document.documentElement.scrollTop;
-        if (scrolled > 300) {
-            setIsVisible(true);
-        } else {
-            setIsVisible(false);
-        }
-    };
-
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-    };
-
-    window.addEventListener("scroll", toggleVisibility);
-
-    return (
-        <botton onClick={scrollToTop} style={{display: isVisible ? 'inline' : 'none'}}> Scroll
-            To Top</botton>
-    );
+function scroll_To_Top() {
+    $(window).scrollTop(0);
 }
-
-const domContainer = document.querySelector('#scroll_to_top_container');
-ReactDOM.render(<ScrollToTop/>, domContainer);
